f023a522d149ebc3bffe3aafc6ef858ca8871316
Dima <38003563+DmitryAvanesov@users.noreply.github.com>
2020-09-25 16:04:10 +0700

    Added sign-out button

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthenticationService } from '../services/authentication.service';
 import { User } from '../types/User';
 
@@ -8,9 +9,17 @@ import { User } from '../types/User';
   styleUrls: ['./test.component.scss'],
 })
 export class TestComponent implements OnInit {
-  constructor(private authenticationService: AuthenticationService) {}
+  constructor(
+    private authenticationService: AuthenticationService,
+    private router: Router
+  ) {}
 
   label: string;
+
+  signOut(): void {
+    localStorage.removeItem('journal-token');
+    this.router.navigate(['/log-in']);
+  }
 
   ngOnInit(): void {}
 }
