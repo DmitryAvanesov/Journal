ac32c86cbf6e5575c87e0e8de92074870a64fdfc
Dima <dima.avanesov@yandex.ru>
2020-09-30 14:31:52 +0700

    Styled header a bit, added a mat-icon

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  ICON_REGISTRY_PROVIDER,
+  MatIconRegistry,
+} from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 import { AuthenticationService } from '../services/authentication.service';
 import { User, UserReqRes } from '../types/User';
 
@@ -8,7 +13,11 @@ import { User, UserReqRes } from '../types/User';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  constructor(private authenticationService: AuthenticationService) {}
+  constructor(
+    private authenticationService: AuthenticationService,
+    private iconRegistry: MatIconRegistry,
+    private sanitizer: DomSanitizer
+  ) {}
 
   user: User | undefined;
 
@@ -20,5 +29,10 @@ export class HeaderComponent implements OnInit {
     this.authenticationService.user.subscribe((user: User | undefined) => {
       this.user = user;
     });
+
+    this.iconRegistry.addSvgIcon(
+      'profile',
+      this.sanitizer.bypassSecurityTrustResourceUrl('../../assets/profile.svg')
+    );
   }
 }
