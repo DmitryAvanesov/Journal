a789860b621b762a6e333636f2b135bcd90ad025
Dima <dima.avanesov@yandex.ru>
2020-09-30 17:27:52 +0700

    Changed header a tags for mat-buttons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
     component: LogInComponent,
     canActivate: [RepeatAuthGuardService],
   },
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuardService] },
+  { path: 'home', component: HomeComponent },
 ];
 
 @NgModule({
