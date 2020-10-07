5c61288d0e11239e0f708ba161f7902a547d899d
Dima <dima.avanesov@yandex.ru>
2020-10-07 17:16:54 +0700

    Fixed sign-out on page refresh

--- src/app/guards/auth-guard.service.ts
+++ src/app/guards/auth-guard.service.ts
@@ -7,6 +7,7 @@ import {
 } from '@angular/router';
 import { User, UserReqRes } from '../types/User';
 import { AuthenticationService } from '../services/authentication.service';
+import { last, skip } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -22,16 +23,11 @@ export class AuthGuardService implements CanActivate {
     _state: RouterStateSnapshot
   ): Promise<boolean> {
     return new Promise((resolve, _reject) => {
-      this.authenticationService.user.subscribe(
-        (res: User | undefined) => {
-          if (res) {
-            return resolve(true);
-          }
-
-          this.router.navigate(['log-in']);
-          return resolve(false);
+      this.authenticationService.getCurrent().subscribe(
+        (res: UserReqRes) => {
+          return resolve(true);
         },
-        (_err: Error) => {
+        (err: Error) => {
           this.router.navigate(['log-in']);
           return resolve(false);
         }
