544bbc1da35d5f72feabc473d580f0512d530a74
Dima <dima.avanesov@yandex.ru>
2020-10-19 14:28:16 +0700

    Publish form w/ No, Year & Cover fields

--- src/app/publish/publish.component.ts
+++ src/app/publish/publish.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-publish',
   templateUrl: './publish.component.html',
-  styleUrls: ['./publish.component.scss']
+  styleUrls: ['./publish.component.scss'],
 })
 export class PublishComponent implements OnInit {
+  constructor(
+    private iconRegistry: MatIconRegistry,
+    private sanitizer: DomSanitizer
+  ) {}
 
-  constructor() { }
+  formGroup: FormGroup;
+
+  submitPublishForm(): void {}
 
   ngOnInit(): void {
-  }
+    this.formGroup = new FormGroup({
+      numberControl: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^\d+$/),
+      ]),
+      yearControl: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^\d+$/),
+        Validators.min(1900),
+        Validators.max(new Date().getFullYear()),
+      ]),
+      coverControl: new FormControl(''),
+    });
 
+    this.iconRegistry.addSvgIcon(
+      'attach',
+      this.sanitizer.bypassSecurityTrustResourceUrl('../../assets/attach.svg')
+    );
+  }
 }
