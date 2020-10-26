91e16f36808347a3f4dc5d554d561de38329e31c
Dima <dima.avanesov@yandex.ru>
2020-10-26 15:38:04 +0700

    Extracted card-header and card-title

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -8,6 +8,8 @@ import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FeatureBlockComponent } from './feature-block/feature-block.component';
 import { CardComponent } from './card/card.component';
+import { CardHeaderComponent } from './card-header/card-header.component';
+import { CardTitleComponent } from './card-title/card-title.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +18,8 @@ import { CardComponent } from './card/card.component';
     FooterComponent,
     FeatureBlockComponent,
     CardComponent,
+    CardHeaderComponent,
+    CardTitleComponent,
   ],
   imports: [
     CommonModule,
@@ -29,6 +33,8 @@ import { CardComponent } from './card/card.component';
     FooterComponent,
     FeatureBlockComponent,
     CardComponent,
+    CardHeaderComponent,
+    CardTitleComponent,
   ],
 })
 export class SharedModule {}
