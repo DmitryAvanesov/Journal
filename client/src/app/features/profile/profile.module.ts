bac4f4e3db31133090ab53150df7fa65865d019b
Dima <dima.avanesov@yandex.ru>
2020-10-26 13:27:06 +0700

    Grouped styles for each component

--- client/src/app/features/profile/profile.module.ts
+++ client/src/app/features/profile/profile.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile.component';
 import { AngularMaterialModule } from 'src/app/angular-material.module';
 import { ProfileRoutingModule } from './profile-routing.module';
+import { DialogDeleteUserComponent } from './dialog-delete-user/dialog-delete-user.component';
 
 @NgModule({
-  declarations: [ProfileComponent],
+  declarations: [ProfileComponent, DialogDeleteUserComponent],
   imports: [CommonModule, AngularMaterialModule, ProfileRoutingModule],
   exports: [ProfileComponent],
 })
